c2445145640a17e9c89babc16876b5af4eafd4dc
pastiland <hello@pastiland.com>
2022-09-18 16:51:00 +0700

    modified:   src/components/Home/index.js modified:   src/components/Video/index.js

--- src/components/Video/index.js
+++ src/components/Video/index.js
@@ -1,13 +1,10 @@
 import React from "react"
-import useMediaQuery from '@mui/material/useMediaQuery';
-import promoDesktop from "../../assets/video/promo-desktop.mp4"
-import promoMobile from "../../assets/video/promo-mobile.mp4"
-import muted from "../../assets/img/video-muted.png"
-import unmuted from "../../assets/img/video-unmuted.png"
-import "./Video.css"
+import promoVideo from "../../assets/video/promo-mobile.mp4";
+import muted from "../../assets/img/video-muted.png";
+import unmuted from "../../assets/img/video-unmuted.png";
+import "./Video.css";
 
 const Video = () => {
-    const matchesMobile = useMediaQuery('(max-width:720px)');
     const [isMuted, setMuted] = React.useState(true)
     React.useEffect(() => {
         var video = document.querySelector('video');
@@ -23,13 +20,9 @@ const Video = () => {
     return <>
         <button className="mute-button" onClick={mute}>{isMuted ? <img src={muted} alt={"muted"}/> : <img src={unmuted} alt="unmuted"/>}</button>
         <div className="video-container">
-            {matchesMobile ?
-                <video width={'100%'} autoPlay playsInline muted loop>
-                    <source src={promoMobile} type="video/mp4" />
-                </video> 
-                : <video width={'100%'} autoPlay muted loop>
-                    <source src={promoDesktop} type="video/mp4" />
-                </video>}
+            <video width={'100%'} autoPlay playsInline muted loop>
+                <source src={promoVideo} type="video/mp4" />
+            </video>
         </div>
     </>
 

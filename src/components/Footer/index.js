85daad65b74053151cdb4cc22b32ce33b5c90c36
pastiland <hello@pastiland.com>
2022-09-19 21:45:28 +0700

    modified:   package-lock.json modified:   package.json modified:   src/App.css modified:   src/components/Footer/index.js modified:   src/components/Header/Header.css

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -3,16 +3,30 @@ import social_twitter from "../../assets/img/social-twitter.png";
 import social_discord from "../../assets/img/social-discord.png";
 import social_instagram from "../../assets/img/social-instagram.png";
 import Logo_bottom from "../../assets/img/bottom-logo.png";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTwitter } from "@fortawesome/free-brands-svg-icons";
+import { faDiscord } from "@fortawesome/free-brands-svg-icons";
+import { faInstagram } from "@fortawesome/free-brands-svg-icons";
+
 import "./Footer.css";
 
 const Footer = () => {
     return <> 
         <div className="footer-banner"></div>
         <div className="footer-container">
+            {/*
             <div className="footer-social">
                 <a href="https://twitter.com/clit_opia" rel="noreferrer" target="_blank"><img src={social_twitter} alt="Twitter" /></a>
                 <a href="https://discord.com/clitopia" rel="noreferrer" target="_blank"><img src={social_discord} alt="Discord" /></a>
                 <a href="https://instagram.com/clitopia" rel="noreferrer" target="_blank"><img src={social_instagram} alt="Instagram" /></a>
+            </div>
+*/}
+            <div className="footer-social">
+                <ul>
+                    <li><a href="#"><FontAwesomeIcon icon={faTwitter} className="icon" /></a></li>
+                    <li><a href="#"><FontAwesomeIcon icon={faDiscord} className="icon" /></a></li>
+                    <li><a href="#"><FontAwesomeIcon icon={faInstagram} className="icon" /></a></li>
+                </ul>
             </div>
             <div className="footer-text">❤ Join the community for more info ❤</div>
             <div className="footer-logo">

834391a7c3527f2387b74ec4675404acc2940df7
pastiland <hello@pastiland.com>
2022-09-26 04:46:50 +0700

    modified:   src/components/About/About.css modified:   src/components/Footer/Footer.css modified:   src/components/Footer/index.js modified:   src/components/Header/Header.css modified:   src/components/Home/Home.css modified:   src/components/Legendary/Legendary.css modified:   src/components/Manifesto/Manifesto.css

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -13,9 +13,10 @@ const Footer = ({ handleClick }) => {
         <div className="footer-container">
             <div className="footer-social">
                 <ul>
-                    <li><a href="https://twitter.com/clit_opia" rel="noreferrer" target="_blank"><FontAwesomeIcon icon={faTwitter} className="icon" /></a></li>
-                    <li><a href="https://discord.com/clitopia" rel="noreferrer" target="_blank"><FontAwesomeIcon icon={faDiscord} className="icon" /></a></li>
-                    <li><a href="https://instagram.com/clitopia" rel="noreferrer" target="_blank"><FontAwesomeIcon icon={faInstagram} className="icon" /></a></li>
+                    
+                <li><a href="https://discord.com/clitopia" rel="noreferrer" target="_blank"><FontAwesomeIcon icon={faDiscord} className="icon" title="Join us on Discord"/></a></li>
+                    <li><a href="https://twitter.com/clit_opia" rel="noreferrer" target="_blank"><FontAwesomeIcon icon={faTwitter} className="icon" title="Follow us on Twitter" /></a></li>
+                    <li><a href="https://instagram.com/clitopia" rel="noreferrer" target="_blank"><FontAwesomeIcon icon={faInstagram} className="icon" title="Follow us on Instagram" /></a></li>
                 </ul>
             </div>
             <div className="footer-text">Join the community for more info!</div>
